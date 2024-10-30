421678da739897bf4234ec199caf686d16913232
Logan <3116115335@qq.com>
2024-10-30 15:46:15 +0800

    根据项目精简初始化项目

---
+++ la-la-land/src/main/java/com/lala/land/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.lala.land.mapper;
+
+import com.lala.land.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author <a href="https://github.com/lilala">程序员鱼皮</a>
+ * @from <a href="https://lala.icu">编程导航知识星球</a>
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ la-la-land/src/main/java/com/lala/land/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.lala.land.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lala.land.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/lilala">程序员鱼皮</a>
+ * @from <a href="https://lala.icu">编程导航知识星球</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
